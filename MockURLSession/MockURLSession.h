8593ce77e7b504640674ebe35245db12352ac049
Kenta Yamamoto <ymkjp@users.noreply.github.com>
2016-08-24 13:09:14 +0900

    Initial Commit

---
+++ MockURLSession/MockURLSession.h
@@ -0,0 +1,19 @@
+//
+//  MockURLSession.h
+//  MockURLSession
+//
+//  Created by YAMAMOTOKenta on 8/24/16.
+//  Copyright © 2016 ymkjp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MockURLSession.
+FOUNDATION_EXPORT double MockURLSessionVersionNumber;
+
+//! Project version string for MockURLSession.
+FOUNDATION_EXPORT const unsigned char MockURLSessionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MockURLSession/PublicHeader.h>
+
+
